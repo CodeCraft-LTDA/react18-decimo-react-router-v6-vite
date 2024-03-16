8dc262208326c4e9374efe785608295ef58f4be6
Sidnei <sidneisonic@hotmail.com>
2024-03-16 19:51:06 -0300

    aula 94lidando com parametros de consulta

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Products = () => {
 
@@ -8,10 +9,33 @@ const Products = () => {
         navigate("/products/1/2");
     }
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        //METHOD 1: ele exclui todos os queries strings
+        // setSearchParams({ count: count.toString() });
+
+        //METHOD 2: ele mantem os queries strings
+        // searchParams.set("count", count.toString());
+        // setSearchParams(searchParams);
+
+        console.log(searchParams.get("count"));
+        console.log(searchParams.has("count"));
+        
+        searchParams.delete("count");
+        setSearchParams(searchParams);
+    }, [count]);
+
+    const handleClicked = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
             <h1>Products</h1>
             <button onClick={handleClick}>Product 1</button>
+            <button onClick={handleClicked}>Clicked {count} times</button>
         </div>
     );
 }
